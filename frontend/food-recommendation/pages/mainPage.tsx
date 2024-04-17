9abdb90cfbbd499ad5beebab98860092be16ec2c
rpate136 <rishiapatel111@gmail.com>
2024-04-16 22:51:47 -0400

    tailwind setup

---
+++ frontend/food-recommendation/pages/mainPage.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+
+const message = () => {
+
+    return(
+
+        <div className="bg-black-700 h-screen flex justify-center items-center">
+            
+        <div className="text-center">
+            <h1 className="text-4xl font-bold text-white mb-2 rainbow-text animate-pulse">Coming Soon</h1>
+        </div>
+
+        </div>
+
+    );
+};
+
+export default message;
